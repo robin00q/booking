c016b9d8bc8cc1e481e50588748d6d6776a8ba89
Sukjune Lee <robin00q@naver.com>
2020-02-06 20:20:37 +0900

    Add JunitTest (Category, Promotion)

---
+++ src/test/java/com/naver/booking/dao/CategoryDaoTest.java
@@ -0,0 +1,33 @@
+package com.naver.booking.dao;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import com.naver.booking.config.ApplicationConfiguration;
+import com.naver.booking.dto.CategoryDto;
+
+class CategoryDaoTest {
+	
+	@Autowired
+	ApplicationContext ac = new AnnotationConfigApplicationContext(ApplicationConfiguration.class);
+	
+	CategoryDao categoryDao = ac.getBean(CategoryDao.class);
+
+	@Test
+	void testSelectAll() {
+		List<CategoryDto> categoryDtoList = new ArrayList<CategoryDto>();
+		
+		categoryDtoList = categoryDao.selectAll();
+		
+		assertNotNull(categoryDtoList);
+	}
+
+}
