fa3f87a871063f3fee22ff4138962c8f9f35e687
Sukjune Lee <robin00q@naver.com>
2020-02-09 21:06:54 +0900

    Add Main Controller and testing use main.jsp

--- src/main/java/com/naver/booking/controller/MainController.java
+++ src/main/java/com/naver/booking/controller/MainController.java
@@ -1,13 +1,43 @@
 package com.naver.booking.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import com.naver.booking.dto.CategoryDto;
+import com.naver.booking.dto.ProductDto;
+import com.naver.booking.dto.PromotionDto;
+import com.naver.booking.service.CategoryService;
+import com.naver.booking.service.ProductService;
+import com.naver.booking.service.PromotionService;
 
 @Controller
 public class MainController {
 
+	@Autowired
+	PromotionService promotionService;
+	
+	@Autowired
+	CategoryService categoryService;
+	
+	@Autowired
+	ProductService productService;
+	
 	@GetMapping(path="/")
-	public String mainPage() {
+	public String mainPage(ModelMap modelMap) {
+
+		int initialCategoryId = 1;
+		
+		List<PromotionDto> promotionList = promotionService.getPromotionsIdProductIdSaveFileName();
+		List<CategoryDto> categoryList = categoryService.getCategoriesIdNameCount();
+		List<ProductDto> productList = productService.getProductsDisplayInfoByCategoryId(initialCategoryId, 0);
+		
+		modelMap.put("promotionList", promotionList);
+		modelMap.put("categoryList", categoryList);
+		modelMap.put("productList", productList);
 		
 		return "main";
 	}
--- src/main/webapp/WEB-INF/views/main.jsp
+++ src/main/webapp/WEB-INF/views/main.jsp
@@ -1,5 +1,6 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
 <!DOCTYPE html>
 <html>
 <head>
@@ -7,6 +8,16 @@
 <title>Insert title here</title>
 </head>
 <body>
-hello
+
+<c:forEach items="${promotionList}" var="promotionList">
+	${promotionList.toString()}</br>
+</c:forEach>
+<c:forEach items="${categoryList}" var="categoryList">
+	${categoryList.toString()}</br>
+</c:forEach>
+<c:forEach items="${productList}" var="productList">
+	${productList.toString()}</br>
+</c:forEach>
+
 </body>
 </html>
