75f2703e6fb8f3680f932d0629931866c2489394
Sukjune Lee <robin00q@naver.com>
2020-02-14 23:58:42 +0900

    Remove files don't needed

--- src/main/java/com/naver/booking/dao/DisplayInfoDaoSqls.java
+++
@@ -1,8 +0,0 @@
-package com.naver.booking.dao;
-
-public class DisplayInfoDaoSqls {
-
-	public static final String SELECT_DISPLAY_INFO_TOTAL_COUNT = 
-			"SELECT COUNT(*) "
-			+ "FROM display_info";
-}
