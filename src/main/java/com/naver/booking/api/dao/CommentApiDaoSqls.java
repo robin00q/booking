7ecb7c7beefcfc4e5ed65825a7f7bbb9f303e3fb
Sukjune Lee <robin00q@naver.com>
2020-02-18 14:46:23 +0900

    Add Comment Api Dao, Dto, Test(not implemented yet)

---
+++ src/main/java/com/naver/booking/api/dao/CommentApiDaoSqls.java
@@ -0,0 +1,34 @@
+package com.naver.booking.api.dao;
+
+public class CommentApiDaoSqls {
+	
+	public static final String SELECT_COMMENT_API = 
+			"SELECT " + 
+			"reservation_user_comment.id AS comment_id, " + 
+			"reservation_user_comment.product_id AS product_id, " + 
+			"reservation_user_comment.reservation_info_id AS reservation_info_id, " + 
+			"reservation_user_comment.score AS score, " + 
+			"reservation_user_comment.comment AS comment, " + 
+			"reservation_info.reservation_name AS reservation_name, " + 
+			"reservation_info.reservation_tel AS reservation_telephone, " + 
+			"reservation_info.reservation_email AS reservation_email, " + 
+			"reservation_info.reservation_date AS reservation_date, " + 
+			"reservation_user_comment.create_date AS create_date, " + 
+			"reservation_user_comment.modify_date AS modify_date " + 
+			"FROM reservation_user_comment " + 
+			"INNER JOIN reservation_info " + 
+			"ON reservation_info.id = reservation_user_comment.reservation_info_id"; 
+	
+	public static final String SELECT_COMMENT_API_WITH_COMMENTS = 
+			"SELECT * " + 
+			"FROM (" + 
+			SELECT_COMMENT_API	 + 
+			") AS joined1 " + 
+			"LEFT OUTER JOIN " + 
+			"(" + 
+			CommentImageApiDaoSqls.SELECT_COMMENT_IMAGE_API + 
+			") AS comment_images " + 
+			"ON joined1.reservation_info_id = comment_images.reservation_info_id";
+
+	
+}
---
+++ src/test/java/com/naver/booking/api/dao/CommentApiDaoTest.java
@@ -0,0 +1,47 @@
+package com.naver.booking.api.dao;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.fail;
+
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import com.naver.booking.api.dto.CommentApiDto;
+import com.naver.booking.config.ApplicationConfiguration;
+
+class CommentApiDaoTest {
+	
+	@Autowired
+	ApplicationContext ac = new AnnotationConfigApplicationContext(ApplicationConfiguration.class);
+	
+	CommentApiDao commentApiDao = ac.getBean(CommentApiDao.class);
+
+	@Test
+	void testSelectCommentApi() {
+		
+		List<CommentApiDto> commentApiDtoList = commentApiDao.SelectCommentApi();
+		
+		for (CommentApiDto commentApiDto : commentApiDtoList) {
+			System.out.println(commentApiDto);
+		}
+		
+		assertNotNull(commentApiDtoList);
+	}
+	
+//	@Test
+//	void testSelectCommentApiWithComments() {
+//		
+//		List<CommentApiDto> commentApiDtoList = commentApiDao.SelectCommentApiWithComments();
+//		
+//		for (CommentApiDto commentApiDto : commentApiDtoList) {
+//			System.out.println(commentApiDto);
+//		}
+//		
+//		assertNotNull(commentApiDtoList);
+//	}
+
+}
