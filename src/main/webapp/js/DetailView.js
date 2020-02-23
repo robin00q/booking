6d033d22b19ab54ed275aa66c988c37a8388524c
Sukjune Lee <robin00q@naver.com>
2020-02-23 16:41:18 +0900

    add MoreButton eventlistener

--- src/main/webapp/js/DetailView.js
+++ src/main/webapp/js/DetailView.js
@@ -40,6 +40,26 @@ var addSlideButtonEventListener = {
 	}
 }
 
+var addBkMoreButtonEventListener = {
+	openButton : function(){
+		document.querySelector("._open").addEventListener('click', function(){
+		    document.querySelector(".store_details").classList.remove('close3');
+		    this.style.display = 'none';
+		    document.querySelector("._close").style.display = 'block';
+		}, false)
+	},
+	closeButton : function(){
+		document.querySelector("._close").addEventListener('click', function(){
+		    document.querySelector(".store_details").classList.add('close3');
+		    this.style.display = 'none';
+		    document.querySelector("._open").style.display = 'block';
+		}, false)
+	} 
+}
+
 visualizeNumOffButton.initializeButton();
 addSlideButtonEventListener.slidePrev(document.querySelector("#slide_images"));
 addSlideButtonEventListener.slideNext(document.querySelector("#slide_images"));
+addBkMoreButtonEventListener.openButton();
+addBkMoreButtonEventListener.closeButton();
+
