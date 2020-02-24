dd65a1027577f5cbfda271193afba92bdd64acf9
Sukjune Lee <robin00q@naver.com>
2020-02-24 22:26:11 +0900

    Add score, date, comment at review.jsp

--- src/main/java/com/naver/booking/controller/ReviewController.java
+++ src/main/java/com/naver/booking/controller/ReviewController.java
@@ -1,14 +1,31 @@
 package com.naver.booking.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import com.naver.booking.dto.CommentDto;
+import com.naver.booking.service.CommentService;
+
 @Controller
 public class ReviewController {
+	
+	@Autowired
+	CommentService commentService;
 
 	@GetMapping(path="/review/{productId}")
-	public String reviewPage(@PathVariable("productId") Long productId) {
+	public String reviewPage(@PathVariable("productId") Long productId, ModelMap modelMap) {
+		
+		List<CommentDto> commentDtoList = commentService.getThreeComment(productId);
+		double averageScore = commentService.getAverageScore(productId);
+		
+		modelMap.put("commentDtoList", commentDtoList);
+		modelMap.put("averageScore", averageScore);
+
 		return "review";
 	}
 }
