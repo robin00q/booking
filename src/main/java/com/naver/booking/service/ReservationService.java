2d40d7da14d878b6053d8bfddf1b1375c55b3d0f
Sukjune Lee <robin00q@naver.com>
2020-03-03 20:15:28 +0900

    Add ProductPriceService

--- src/main/java/com/naver/booking/service/ReservationService.java
+++ src/main/java/com/naver/booking/service/ReservationService.java
@@ -1,8 +1,13 @@
 package com.naver.booking.service;
 
+import java.util.List;
+
 import com.naver.booking.dto.DisplayInfoImageDto;
+import com.naver.booking.dto.ProductPriceDto;
 
 public interface ReservationService {
 
 	public DisplayInfoImageDto getDisplayInfoImage(long displayInfoId);
+	
+	public List<ProductPriceDto> getProductPriceByProductId(long productId);
 }
--- src/main/java/com/naver/booking/service/impl/ReservationServiceImpl.java
+++ src/main/java/com/naver/booking/service/impl/ReservationServiceImpl.java
@@ -1,10 +1,14 @@
 package com.naver.booking.service.impl;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.naver.booking.dao.DisplayInfoImageDao;
+import com.naver.booking.dao.ProductPriceDao;
 import com.naver.booking.dto.DisplayInfoImageDto;
+import com.naver.booking.dto.ProductPriceDto;
 import com.naver.booking.service.ReservationService;
 
 @Service
@@ -13,9 +17,20 @@ public class ReservationServiceImpl implements ReservationService {
 	@Autowired
 	DisplayInfoImageDao displayInfoImageDao;
 	
+	@Autowired
+	ProductPriceDao productPriceDao;
+	
 	@Override
 	public DisplayInfoImageDto getDisplayInfoImage(long displayInfoId) {
 		return displayInfoImageDao.selectDisplayInfoImage(displayInfoId);
 	}
+
+	@Override
+	public List<ProductPriceDto> getProductPriceByProductId(long productId) {
+		return productPriceDao.selectProductPriceByProductId(productId);
+	}
+
+
+
 
 }
